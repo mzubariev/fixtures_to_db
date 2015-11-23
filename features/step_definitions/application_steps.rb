d0fead77b2ce158735ba7ceab2a412043ada486e
nzubariev <nick.zubariev@gmail.com>
2015-11-23 13:56:09 +0200

    Application tests fix

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -2,11 +2,11 @@ Given(/^two input parameters: 'post' and 'ini'$/) do
   save_to_db('ParserIni', 'post')
 end
 When(/^I check post table for a new rows with specific added data \(ini\)$/) do
-  @users = Post.find_by({name: 'post1'})
+  @posts = Post.find_by({name: 'post1'})
 end
 Then(/^I see the rows have been added \(post, ini\)$/) do
-  expect(@users).not_to be_nil
-  expect(@users.length).to be >= 1
+  expect(@posts).not_to be_nil
+  expect(@posts.length).to be >= 1
 end
 
 
@@ -14,11 +14,11 @@ Given(/^two input parameters: 'post' and 'json'$/) do
   save_to_db('ParserJson', 'post')
 end
 When(/^I check post table for a new rows with specific added data \(json\)$/) do
-  @users = Post.find_by({name: 'post1'})
+  @posts = Post.find_by({name: 'post1'})
 end
 Then(/^I see the rows have been added \(post, json\)$/) do
-  expect(@users).not_to be_nil
-  expect(@users.length).to be >= 1
+  expect(@posts).not_to be_nil
+  expect(@posts.length).to be >= 1
 end
 
 
