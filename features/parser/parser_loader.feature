b571ec91e47967035dfe92a448219ab8d5ee5af6
nzubariev <nick.zubariev@gmail.com>
2015-11-23 20:57:21 +0200

    Add parser test

--- features/parser/parser_loader.feature
+++ features/parser/parser_loader.feature
@@ -1,9 +1,9 @@
-Feature: Call the loader Parser factory method
+Feature: Loader of Parser factory method to call one of the two Parser classes
   In order to call loader Parser method
   As a user
   I want to see new Parser object with two fields
 
   Scenario: Check for a new Parser object
-    Given a new object with set fields
+    Given Parser class name and two params for new instance
     When call loader method
     Then I see the new Parser object with two fields
--- features/step_definitions/parser/parser_steps.rb
+++ features/step_definitions/parser/parser_steps.rb
@@ -1,11 +1,14 @@
-# Given(/^a new object with set fields$/) do
-#   @user = User.new({name: 'bob', last_name: 'fisher'})
-# end
-# When(/^call save method$/) do
-#   @user.save
-# end
-# Then(/^I see the row have been added$/) do
-#   user = User.find_by({name: 'bob'})
-#   expect(user).not_to be_nil
-#   expect(user.length).to be >= 1
-# end
+Given(/^Parser class name and two params for new instance$/) do
+  @class_name = 'ParserIni'
+  @path = '../../../datafiles/fixtures/'
+  @file_name = 'user.ini'
+end
+
+When(/^call loader method$/) do
+  @parser = Object.const_get("#{@class_name}").new(@path, @file_name)
+end
+
+Then(/^I see the new Parser object with two fields$/) do
+  expect(@parser.instance_variables.length).to be == 2
+  expect(@parser.class.name).to be == 'ParserIni'
+end
