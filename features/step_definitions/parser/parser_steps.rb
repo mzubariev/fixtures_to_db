ab11af6036f428fd14d6dae73a781022bed1bf9b
Nick Zubariev <nick.zubariev@gmail.com>
2015-11-23 21:04:21 +0200

    Update parser_steps.rb

--- features/step_definitions/parser/parser_steps.rb
+++ features/step_definitions/parser/parser_steps.rb
@@ -3,11 +3,9 @@ Given(/^Parser class name and two params for new instance$/) do
   @path = '../../../datafiles/fixtures/'
   @file_name = 'user.ini'
 end
-
 When(/^call loader method$/) do
   @parser = Object.const_get("#{@class_name}").new(@path, @file_name)
 end
-
 Then(/^I see the new Parser object with two fields$/) do
   expect(@parser.instance_variables.length).to be == 2
   expect(@parser.class.name).to be == 'ParserIni'
